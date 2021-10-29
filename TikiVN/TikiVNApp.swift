0b39b70b7952b90e96ea1f37e52d6d4dee49d197
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2021-10-29 18:10:18 +0700

    Initialization TikiVN

---
+++ TikiVN/TikiVNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikiVNApp.swift
+//  TikiVN
+//
+//  Created by Quang Bao on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TikiVNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
